1842853fabdd157345db39967f9a1a1ed03d5c5c
Рогачевский Иван <ivanrogacevskiy7@gmail.com>
2025-09-08 14:11:02 +0300

    1

--- People/People.cpp
+++ People/People.cpp
@@ -1,7 +1,6 @@
-// People.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "People.h"
+using namespace std;
 
 int main()
 {
---
+++ People/People.h
@@ -0,0 +1,10 @@
+#pragma once
+class People
+{
+	char* name;
+	char* surname;
+	int age;
+public:
+
+};
+
